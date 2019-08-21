04fc66d468338f00d1df27b5dbbd27d4ad826e83
NourAlkandari <46244352+NourAlkandari@users.noreply.github.com>
2019-08-21 11:35:33 +0300

    Add files via upload
    Practicing at work

--- FoundationsProjectOne/shop.py
+++ FoundationsProjectOne/shop.py
@@ -11,8 +11,8 @@ original_price = 2
 signature_price = 2.750
 
 ############################# Start Here! ##############################
-cupcake_shop_name = "yummyy"
-signature_flavors = ["pumbkin", "orange", "carrot"]
+cupcake_shop_name = "Nour-shop"
+signature_flavors = ["pumpkin","mouseroyal","oreo"]
 order_list = []
 
 
@@ -20,18 +20,18 @@ def print_menu():
     """
     Print the items in the menu dictionary.
     """
-    print("Our menu")
+    # your code goes here!
     for item in menu:
-        print("-%s, (%s KWD)" % (item, menu[item]))
-
+        print("- \"%s\": %s" % (item, menu[item]))
 
 def print_originals():
     """
     Print the original flavor cupcakes.
     """
     print("Our original flavor cupcakes (KD %s each):" % original_price)
+    # your code goes here!
     for item in original_flavors:
-        print ("-%s " % (item))
+        print("-\"%s\" " % item)
 
 
 def print_signatures():
@@ -39,22 +39,26 @@ def print_signatures():
     Print the signature flavor cupcakes.
     """
     print("Our signature flavor cupcake (KD %s each):" % signature_price)
+    # your code goes here!
     for item in signature_flavors:
-        print ("-%s" % item)
+        print("-\"%s\" " % item)
 
 
 def is_valid_order(order):
     """
     Check if an order exists in the shop.
     """
+    # your code goes here!
+
     if order.lower() in menu:
         return True
     elif order.lower() in original_flavors:
         return True
     elif order.lower() in signature_flavors:
         return True
-    else: 
+    else:
         return False
+
 
 
 def get_order():
@@ -62,13 +66,15 @@ def get_order():
     Repeatedly ask customer for order until they end their order by typing "Exit".
     """
     order_list = []
-    print ("What's your order? Enter the exact spelling of the item you want. Type 'exit' to end your order.")
-    order = input()
+    # your code goes here!
+    order = input("Please type your order as spelled on the menu, type \"exit\" when you're done \n")
     while order.lower() != "exit":
         if is_valid_order(order):
             order_list.append(order)
-        order = input()
+        else:
+            print("This item is not on the menu, please choose other item, or type \"exit\" to end the order")
 
+        order = input()
 
     return order_list
 
@@ -77,7 +83,7 @@ def accept_credit_card(total):
     """
     Return whether an order is eligible for credit card payment.
     """
-    
+    # your code goes here!
     if total >= 5:
         return True
     else:
@@ -89,12 +95,13 @@ def get_total_price(order_list):
     Calculate and return total price of the order.
     """
     total = 0
-    for order in order_list:
-        if order.lower() in menu:
-            total += menu[order.lower()]
-        elif order.lower() in original_flavors:
+    # your code goes here!
+    for item in order_list:
+        if item in menu:
+            total +=  menu[item]
+        elif item in original_flavors:
             total += original_price
-        elif order.lower() in signature_flavors:
+        elif item in signature_flavors:
             total += signature_price
 
     return total
@@ -104,9 +111,17 @@ def print_order(order_list):
     """
     Print the order of the customer.
     """
+    total = get_total_price(order_list)
     print()
     print("Your order is: ")
-    for order in order_list:
-        print ("-%s"% order)
-    print ("That's will be KWD: %s" % get_total_price(order_list))
-    print ("Thank you for shopping at yummyy")
+    # your code goes here!
+    for item in order_list:
+        print ( "\"%s\"" % item )
+
+    
+    print ("Total Price = \"%s\" " % get_total_price(order_list))
+    
+    if accept_credit_card(total):
+        print ("This order eligible for credit card")
+    else:
+        print ("Sorry!! This order is not eligible for credit card, you're total price must reach 5:00KD")
